676d9193ad8f851ae4f7e797462ed110eac10361
kunalgarg <kunalgarg1213@gmail.com>
2022-07-01 15:47:03 +0530

    home page added

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express();
 //     console.log("we got another request");
 //     res.send("got your request ")
 // })
+app.get('/', (req, res) => {
+    console.log('home page ');
+    res.send('welcome to home page')
+})
 app.get("/cats", (req, res) => {
     console.log("we got cat request");
-    res.send("got your cat request ")
+    res.send("MEOW ");
 })
 app.listen(3000, () => {
     console.log("listening on Port no 3000");
