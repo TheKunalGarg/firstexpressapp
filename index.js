6e6adebf135adbcee27dae95cfea4c581458f7fd
kunalgarg <kunalgarg1213@gmail.com>
2022-07-01 15:44:00 +0530

    cat request made

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 // console.dir(app);
-app.use((req, res) => {
-    console.log("we got another request");
-    res.send("got your request ")
+// app.use((req, res) => {
+//     console.log("we got another request");
+//     res.send("got your request ")
+// })
+app.get("/cats", (req, res) => {
+    console.log("we got cat request");
+    res.send("got your cat request ")
 })
 app.listen(3000, () => {
     console.log("listening on Port no 3000");
