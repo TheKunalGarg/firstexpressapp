1125b7f24b9c05b35331e79feafbe0329a497ec3
kunalgarg <kunalgarg1213@gmail.com>
2022-07-02 23:56:45 +0530

    second commit

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.get("/cats", (req, res) => {
     console.log("we got cat request");
     res.send("MEOW ");
 })
+app.get('*', (req, res) => {
+    res.send("I don't know the path");
+})
 app.listen(3000, () => {
     console.log("listening on Port no 3000");
 })
